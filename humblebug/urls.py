f54e83b1390fa54d3ae01495bec49bfea6a51475
jake <tkddnd82@gmail.com>
2021-08-20 13:50:25 +0900

    Add:소셜로그인(카카오)기능 추가 - 카카오 로그인 기능 추가 - unit test 완료 - test.py

--- humblebug/urls.py
+++ humblebug/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("projects.urls")),
+    path('users', include('users.urls')),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+import jwt
+import requests
+import bcrypt
+
+from django.views          import View
+from django.http           import JsonResponse, response
+
+from users.models          import User
+from humblebug.settings    import SECRET_KEY
+from my_settings           import AlGORITHM
+
+from django.shortcuts import render
+
+# Create your views here.
+import json
+import jwt
+import requests
+import bcrypt
+
+from django.views          import View
+from django.http           import JsonResponse, response
+
+from users.models          import User
+from humblebug.settings    import SECRET_KEY
+from my_settings           import AlGORITHM
+
+class KakaologinView(View):
+    def get(self, request):
+        try:
+            access_token = request.headers.get("Authorization")
+            
+            if not access_token:
+                return JsonResponse({"message" : "INVALID_TOKEN"}, status = 400)
+                
+            response = requests.get(
+                    "https://kapi.kakao.com/v2/user/me", 
+                    headers = {"Authorization" : f"Bearer {access_token}"}
+                    )
+                    
+            if not response:
+                    return JsonResponse({"message" : "NOT_FOUND_IN_KAKAO"}, status = 404)
+                    
+            profile_json  = response.json()
+                
+            kakao_id      = profile_json.get("id")
+            kakao_account = profile_json.get("kakao_account")
+            name          = kakao_account["profile"]["nickname"]
+                
+            user, created = User.objects.get_or_create(
+			kakao     = kakao_id,
+			defaults  = {"nickname" : name,}
+			)
+            user.name     = name
+            user.save()
+			
+            token = jwt.encode({"id" : user.id}, SECRET_KEY, algorithm = "HS256")
+                
+            return JsonResponse({"message" : "SUCCESS", "acess_token" : token}, status = 200)
+        except KeyError:
+                return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
