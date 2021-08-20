df693454f9672906d1d1a2a60ad7f8cb1c8b9eb3
lunayyko <youryoung.ko@gmail.com>
2021-08-20 13:16:58 +0900

    ADD: 메인 리스트(필터) 뷰, 테스트 코드

--- projects/views.py
+++ projects/views.py
@@ -11,8 +11,9 @@ from projects.models        import Project, Patron, Option, Category, Tag, Comme
 from utils import log_in_confirm
 
 class ProjectListView(View):
+    @query_debugger
     def get(self, request):
-
+        today = datetime.now()
         category_id = request.GET.get("categoryId")
         
         q = Q()
@@ -23,18 +24,25 @@ class ProjectListView(View):
         limit        = int(request.GET.get('limit', 12))
         offset       = int(request.GET.get('offset', 0))
 
+        projects = Project.objects.annotate(total=Sum('patron__total_amount'), count=Count('patron')).prefetch_related('tag').select_related('user', 'category').filter(q).order_by('created_at')[offset:offset+limit]
+
         projects = [{
-                "id"             : project.id,
-                "category_id"    : project.category_id,
-                "user_id"        : project.user_id,
-                "user_name"      : project.user.nickname,
-                "name"           : project.name,
-                "image"          : project.main_image_url,
-                "aim_amount"     : project.aim_amount,
-                "created_at"     : project.created_at,
-                "end_date"       : project.end_date, 
-                "description"    : project.description
-            } for project in Project.objects.filter(q).order_by('created_at')[offset:offset+limit]]
+                'tag'             : [{'id':tag.id, 'name':tag.tag} for tag in project.tag.all()],
+                'name'            : project.name,
+                'id'              : project.id,
+                'user_id'         : project.user_id,
+                'user'            : project.user.nickname,
+                'remaining_time'  : (project.end_date.replace(tzinfo=None) - today).days,
+                'main_image_url'  : project.main_image_url,
+                'category_name'   : project.category.name,
+                "aim_amount"      : project.aim_amount,
+                'payment_date'    : project.end_date.strftime("%Y년 %m월 %d일"),
+                'collected_amount': project.total,
+                'percentage' : '%.f%%'%(project.total/project.aim_amount*100) if project.total else '0%',
+                "created_at"      : project.created_at,
+                "end_date"        : project.end_date, 
+                "description"     : project.description
+            } for project in projects]
             
         return JsonResponse({"project" : projects}, status=200)
 
