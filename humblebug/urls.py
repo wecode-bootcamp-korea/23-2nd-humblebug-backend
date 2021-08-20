3ad54760946c0f66e8d6cb5767da7868b2cfbc70
lunayyko <youryoung.ko@gmail.com>
2021-08-20 13:16:58 +0900

    ADD: 메인 리스트(필터) 뷰, 테스트 코드

--- humblebug/urls.py
+++ humblebug/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("projects.urls")),
-    path("project", include("projects.urls")),
     path('users', include('users.urls')),
 ]
