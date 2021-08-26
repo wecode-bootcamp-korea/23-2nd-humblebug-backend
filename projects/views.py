a3ab84d5be0a2afa992d9d046e36d61e50735dd2
lordmyshepherd <lordmyshepherd29@gmail.com>
2021-08-26 14:48:27 +0900

    WIP

--- projects/views.py
+++ projects/views.py
@@ -1,100 +1,61 @@
-import json
-import jwt
-import requests
-import bcrypt
+import logging
 
-from datetime import datetime
+logger = logging.getLogger()
 
-from django.views           import View
-from django.http            import JsonResponse
-from django.db.models       import Q, Sum, Count,Avg
-from django.core.exceptions import FieldError
+class ProjectView():
+    @login_decorator
+    def post(self, request):
+        try:
+            user = request.user
+            data = request.POST
+            cloud_storage = CloudStorage(ACCESS_KEY_ID, SECRET_ACCESS_KEY, bucket)
 
-from projects.models        import Category, Project, Tag
-from users.models           import User
-from humblebug.settings     import SECRET_KEY
-class ProjectListView(View):
-    def get(self, request):
+            image = request.FILES.get('image')
+            input_date = data.get('end_date')
 
-        category_id = request.GET.get("categoryId")
-        
-        q = Q()
+            if not image:
+                return JsonResponse({'MESSAGE' : 'IMAGE_EMPTY'}, status=400)
 
-        if category_id:
-            q &= Q(category_id = category_id)
-        
-        limit        = int(request.GET.get('limit', 12))
-        offset       = int(request.GET.get('offset', 0))
 
-        projects = [{
-                "id"             : project.id,
-                "category_id"    : project.category_id,
-                "user_id"        : project.user_id,
-                "user_name"      : project.user.nickname,
-                "name"           : project.name,
-                "image"          : project.main_image_url,
-                "aim_amount"     : project.aim_amount,
-                "created_at"     : project.created_at,
-                "end_date"       : project.end_date, 
-                "description"    : project.description
-            } for project in Project.objects.filter(q).order_by('created_at')[offset:offset+limit]]
-            
-        return JsonResponse({"project" : projects}, status=200)
+           Project.objects.create(
+                name           = data.get('name'),
+                user_id        = user.id,
+                aim_amount     = data.get('aim_amount'),
+                description    = data.get('description'),
+                end_date       = input_date[6:10]+'-'+input_date[:2]+'-'+input_date[3:5],
+                category_id    = data.get('category_id'),
+                main_image_url = 'https://humblebug.s3.us-east-2.amazonaws.com/' + upload_key
+            )
 
-class ProjectView(View):
+           cloud_storage.upload_file(file)
+
+            return JsonResponse({'MESSAGE':'SUCCESS'},status = 201)
     
-    def get(self, request, project_id):  
-        today = datetime.now()
+        except KeyError:
+            return JsonResponse({'MESSAGE':'ERROR_INPUT_VALUE'}, status=404) 
 
-        if not Project.objects.filter(id=project_id).exists():
-            return JsonResponse({'message': 'PROJECT_NOT_EXIST'}, status=404)
-      
-        project = Project.objects.annotate(total=Sum('patron__total_amount'), count=Count('patron')).prefetch_related('tag').select_related('user').get(id=project_id) 
+class CloudSotrage:
+    def __init__(self, ACCESS_KEY_ID, SECRET_ACCESS_KEY, bucket):
+        self.ACCESS_KEY_ID = ACCESS_KEY_ID
+        self.SECRET_ACCESS_KEY = SECRET_ACCESS_KEY
+        self.bucket = bucket
+        self.client = boto3.client(
+            's3',
+            aws_access_key_id     = ACCESS_KEY_ID,
+            aws_secret_access_key = SECRET_ACCESS_KEY
+        )
 
-        project_information = {
-            'tag'               : [{'id':tag.id, 'name':tag.tag} for tag in project.tag.all()],
-            'name'              : project.name,
-            'user'              : project.user.nickname,
-            'remaining_time'    : (project.end_date.replace(tzinfo=None) - today).days,
-            'number_of_sponsors': project.count,
-            'main_image_url'    : project.main_image_url,
-            'aim_amount'        : project.aim_amount,
-            'payment_date'      : project.end_date.strftime("%Y년 %m월 %d일"),
-            'collected_amount'  : project.total if project.total else 0,
-            'percentage'        : '%.f%%'%(project.total/project.aim_amount*100) if project.total else '0%',
-        }
+    def upload_file(self, file):
+        unique_id = str(uuid.uuid4()) + file.name
+        try:
+            return self.client.upload_fileobj(
+                file,
+                self.bucket,
+                unique_id,
+                ExtraArgs = {
+                    'ContentType' : image.content_type
+                }
+            )
 
-        return JsonResponse({'project_information':project_information}, status=200)
-class SearchView(View):
-    def get(self, request):
-        search = request.GET.get('search', None)
-        projects = Project.objects.none()
-
-        if Category.objects.filter(name__contains=search).exists():
-            projects = Project.objects.filter(category__name__contains=search)
-
-        if Tag.objects.filter(tag__contains=search).exists():
-            projects = projects.union(Project.objects.filter(tag__tag__contains=search))
-
-        if Project.objects.filter(name__contains=search).exists():
-            projects = projects.union(Project.objects.filter(name__contains=search))
-
-        if Project.objects.filter(description__contains=search).exists():
-            projects = projects.union(Project.objects.filter(description__contains=search))
-
-
-        results = [{
-                    "id"          : project.id,
-                    "id"             : project.id,
-                    "category_id"    : project.category_id,
-                    "user_id"        : project.user_id,
-                    "user_name"      : project.user.nickname,
-                    "name"           : project.name,
-                    "image"          : project.main_image_url,
-                    "aim_amount"     : project.aim_amount,
-                    "created_at"     : project.created_at,
-                    "end_date"       : project.end_date, 
-                    "description"    : project.description,
-            } for project in projects]
-
-        return JsonResponse({'MESSAGE':results}, status=200)
+        except Exception as e:
+            logger.error(f" func.name = {..} | error_message = {e}")
